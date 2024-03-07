9576194f8c0436cf109091244fcfd82f6976b78e
richman0116 <maria8partee@gmail.com>
2024-03-07 13:36:22 -0800

    update race

--- src/components/RaceCard/RaceCardElement.tsx
+++ src/components/RaceCard/RaceCardElement.tsx
@@ -13,69 +13,138 @@ import getWinnerArr from '@/utils/getWinnerArr'
 const RaceCardElement = ({ raceCards, winnerFlag }: IRaceCardElement) => {
   if (winnerFlag === 0)
     return (
-      <Flex
-        marginY={6}
-        width={{ base: '100%', lg: '70%' }}
-        marginX={'auto'}
-        direction={{ base: 'column', md: 'row', lg: 'row' }}
-        justifyContent={'space-between'}
-      >
-        {getWinnerArr(raceCards).map((RaceCard: any, index: number) => {
-          return (
-            <Center
-              marginTop={index === 1 ? 0 : 10}
-              width={{ base: '100%', md: '30%', lg: '30%' }}
-              backgroundColor={'rgba(255, 255, 255, 0.04)'}
-              borderRadius={10}
-              flexDirection={'column'}
-              justifyContent={'space-between'}
-              padding={10}
-              fontSize={16}
-              fontWeight={600}
-              position={'relative'}
-            >
+      <>
+        <Flex
+          marginY={6}
+          width={'70%'}
+          marginX={'auto'}
+          direction={'row'}
+          justifyContent={'space-between'}
+        >
+          {getWinnerArr(raceCards).map((RaceCard: any, index: number) => {
+            return (
               <Center
-                top={-3}
-                position={'absolute'}
-                bgColor="#48BB78"
-                borderRadius={6}
+                marginTop={index === 1 ? 0 : 10}
+                width={'30%'}
+                backgroundColor={'rgba(255, 255, 255, 0.04)'}
+                borderRadius={10}
+                flexDirection={'column'}
+                justifyContent={'space-between'}
+                padding={10}
                 fontSize={16}
-                paddingX={4}
+                fontWeight={600}
+                position={'relative'}
+                display={{ base: 'none', md: 'none', lg: 'block' }}
+                key={index}
               >
-                {index === 0 || index === 1 ? 2 - index : index + 1}th
-              </Center>
-              <Center marginTop={6} flexDirection={'column'}>
-                <Image
-                  width={16}
-                  height={16}
-                  marginRight={2}
-                  borderRadius={10}
-                  src={RaceCard.avatar}
-                />
-                <Text marginTop={2} fontSize={24}>
-                  {RaceCard.name}
-                </Text>
-              </Center>
-              <Center marginTop={6}>
-                <Badge
-                  paddingX={10}
-                  paddingY={2}
+                <Center
+                  top={-3}
+                  position={'absolute'}
+                  bgColor="#48BB78"
                   borderRadius={6}
-                  fontSize={24}
-                  color={'lightgreen'}
-                  fontWeight={800}
+                  fontSize={16}
+                  paddingX={4}
                 >
-                  ${RaceCard.reward}
-                </Badge>
+                  {index === 0 || index === 1 ? 2 - index : index + 1}th
+                </Center>
+                <Center marginTop={6} flexDirection={'column'}>
+                  <Image
+                    width={16}
+                    height={16}
+                    marginRight={2}
+                    borderRadius={10}
+                    src={RaceCard.avatar}
+                  />
+                  <Text marginTop={2} fontSize={24}>
+                    {RaceCard.name}
+                  </Text>
+                </Center>
+                <Center marginTop={6}>
+                  <Badge
+                    paddingX={10}
+                    paddingY={2}
+                    borderRadius={6}
+                    fontSize={24}
+                    color={'lightgreen'}
+                    fontWeight={800}
+                  >
+                    ${RaceCard.reward}
+                  </Badge>
+                </Center>
+                <Center marginTop={6} flexDirection={'column'}>
+                  Opened
+                  <Text>${RaceCard.amount}</Text>
+                </Center>
               </Center>
-              <Center marginTop={6} flexDirection={'column'}>
-                Opened
-                <Text>${RaceCard.amount}</Text>
+            )
+          })}
+        </Flex>
+        <Flex
+          marginY={6}
+          width={{ base: '100%', lg: '70%' }}
+          marginX={'auto'}
+          direction={'column'}
+          justifyContent={'space-between'}
+          display={{ md: 'block', lg: 'none' }}
+        >
+          {raceCards.slice(0, 3).map((RaceCard: any, index: number) => {
+            return (
+              <Center
+                width={'100%'}
+                marginTop={6}
+                backgroundColor={'rgba(255, 255, 255, 0.04)'}
+                borderRadius={10}
+                flexDirection={'column'}
+                justifyContent={'space-between'}
+                padding={10}
+                fontSize={16}
+                fontWeight={600}
+                position={'relative'}
+                key={index}
+              >
+                <Center
+                  top={-3}
+                  position={'absolute'}
+                  bgColor="#48BB78"
+                  borderRadius={6}
+                  fontSize={16}
+                  paddingX={4}
+                >
+                  {index + 1}th
+                </Center>
+                <Center marginTop={6} flexDirection={'column'}>
+                  <Image
+                    width={16}
+                    height={16}
+                    marginRight={2}
+                    borderRadius={10}
+                    src={RaceCard.avatar}
+                  />
+                  <Text marginTop={2} fontSize={24}>
+                    {RaceCard.name}
+                  </Text>
+                </Center>
+                <Center marginTop={6}>
+                  <Badge
+                    paddingX={10}
+                    paddingY={2}
+                    borderRadius={6}
+                    fontSize={24}
+                    color={'lightgreen'}
+                    fontWeight={800}
+                  >
+                    ${RaceCard.reward}
+                  </Badge>
+                </Center>
+                <Center marginTop={6} flexDirection={'column'}>
+                  Opened
+                  <Text>${RaceCard.amount}</Text>
+                </Center>
               </Center>
-            </Center>
-          )
-        })}
-      </Flex>
+            )
+          })}
+        </Flex>
+      </>
     )
   else
     return (
@@ -92,12 +161,13 @@ const RaceCardElement = ({ raceCards, winnerFlag }: IRaceCardElement) => {
               fontSize={16}
               fontWeight={600}
               position={'relative'}
+              key={index}
             >
               <Badge
                 left={-8}
                 position={'absolute'}
                 colorScheme="grey"
-                fontSize={16}
+                fontSize={{ base: 12, md: 16 }}
                 paddingX={4}
               >
                 {index + 4}th
