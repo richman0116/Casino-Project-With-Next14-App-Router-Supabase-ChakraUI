20fb7cd345c2bc7ba81a7b53902f7e9abfdc1fe9
richman0116 <maria8partee@gmail.com>
2024-03-14 17:15:44 -0500

    Implement cart and deposit modals

---
+++ src/components/CartModal/CartModal.tsx
@@ -0,0 +1,31 @@
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  ModalCloseButton,
+  ModalHeader,
+} from '@chakra-ui/react'
+
+interface ICartModal {
+  isOpen: boolean
+  onClose: () => void
+  children: React.ReactNode
+}
+
+const CartModal = ({ isOpen, onClose, children }: ICartModal) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent bgColor={'#1F1F1F'} maxW={{ base: '350', md: '500', lg: '672' }}>
+        <ModalHeader>Quick Cart</ModalHeader>
+        <ModalBody pt={8} pb={6} px={6}>
+          {children}
+        </ModalBody>
+        <ModalCloseButton />
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default CartModal
---
+++ src/constants/cryptoSelect.ts
@@ -0,0 +1,44 @@
+import { CryptoSelect } from '@/types/cryptoSelect'
+
+export const CRYPTO_SELECTS: Array<CryptoSelect> = [
+  {
+    imageURL: '/assets/images/cryptobrand/BTC.webp',
+    label: 'BTC',
+    name: 'BTC',
+  },
+  {
+    imageURL: '/assets/images/cryptobrand/ETH.webp',
+    label: 'ETH',
+    name: 'ETH',
+  },
+  {
+    imageURL: '/assets/images/cryptobrand/LTC.webp',
+    label: 'LTC',
+    name: 'LTC',
+  },
+  {
+    imageURL: '/assets/images/cryptobrand/XRP.webp',
+    label: 'XRP',
+    name: 'XRP',
+  },
+  {
+    imageURL: '/assets/images/cryptobrand/DOGE.webp',
+    label: 'DOGE',
+    name: 'DOGE',
+  },
+  {
+    imageURL: '/assets/images/cryptobrand/USDT.webp',
+    label: 'USDT',
+    name: 'USDT',
+  },
+  {
+    imageURL: '/assets/images/cryptobrand/SHIB.webp',
+    label: 'SHIB',
+    name: 'SHIB',
+  },
+  {
+    imageURL: '/assets/images/cryptobrand/USDC.webp',
+    label: 'USDC',
+    name: 'USDC',
+  },
+]
